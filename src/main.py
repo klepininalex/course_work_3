789e44529e0f2070e57061883a5af69df503cf70
klepininalex <klepininalex@gmail.com>
2023-06-18 15:36:24 +0300

    added code in main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import utils
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main():
+    data = utils.load_data('operations.json')
+    operations = utils.get_clear_data(data)
+
+    user_input = int(input('Введите количество последних операций, которые хотите увидеть: '))
+
+    executed_operations = utils.get_last_operations(operations, user_input)
+    date = utils.get_datetime(executed_operations)
+    description = utils.get_description_operations(executed_operations)
+    sender = utils.hide_card_number_sender(utils.get_transfer_from(executed_operations))
+    payee = utils.hide_card_number_payee(utils.get_transfer_to(executed_operations))
+    amount = utils.get_operation_amount(executed_operations)
+    currency = utils.get_operation_currency(executed_operations)
+
+    for i in range(user_input):
+        print(f'{date[i]} {description[i]}\n'
+              f'{sender[i]} > {payee[i]}\n'
+              f'{amount[i]} {currency[i]}')
+        print()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
